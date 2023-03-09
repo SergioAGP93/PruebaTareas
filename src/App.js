474af9a8ede78caa006e2e0ace873d6029b991fa
Sergio <zergiogp@gmail.com>
2023-03-09 20:38:05 -0300

    Apliacion de agregar tareas V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+function App () {
+
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+
+  function handleRemoveItem() {
+    const newItems = [...items];
+    newItems.splice(newItems.length - 1, 1);
+    setItems(newItems);
+
+
+  };
+
+  function handleAddItem() {
+    setItems([...items, <li key={items.length}>{name} </li>]);
+
+    setName('')
+
+  };
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+    
+    return (
+      <div>
+        <h1 className='command'>Aplicación de tareas</h1>
+      <div className='command'>
+        <input type='text' value={name} onChange={handleNameChange} placeholder="Nombre de la lista"></input>
+        <button onClick={handleAddItem} >Agregar elemento </button>
+        <button onClick={handleRemoveItem} > Borrar ultimo elemento</button>
+      </div>
+
+      <ul className='lista'>
+        {items}
+      </ul>
+      </div>
+    );
+  }
+
 
 export default App;
